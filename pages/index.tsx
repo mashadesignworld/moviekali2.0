1c5c4bfe5e2b246e1597529d77a21ebf1406e2da
mashadesignworld <njugunak.kevin@gmail.com>
2024-06-27 01:29:06 +0300

    latet commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,40 @@
 
 import Image from "next/image";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+import useCurrentUser from "hooks/useCurrentUser";
+
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+
+}
+
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
   return (
 
     <div className="relative h-full w-full bg-[url('/images/hero3.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
           <h1 className="text-4xl font-bold underline text-white">MovieKali</h1>
+          <p className="text-white">Logged in as : {user?.email}</p>
+          <button className="h-10 w-full bg-white"
+            onClick={() => signOut()}>Log Out</button>
         </nav>
       </div>
     </div>
---
+++ pages/profiles.tsx
@@ -0,0 +1,9 @@
+const Profiles = () => {
+    return (
+        <div className="bg-black w-full h-full">
+            <p className="text-white text-4xl">Profiles</p>
+        </div>
+    );
+};
+
+export default Profiles;
